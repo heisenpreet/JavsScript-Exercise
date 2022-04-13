73faafaf8a00f7fc6859f1c777c33feac4ea091f
Harpreet Singh <harpreetsinghsodi152@gmail.com>
2022-04-13 18:05:20 +0530

    v5

--- 17. Classes and prototypes/script.js
+++ 17. Classes and prototypes/script.js
@@ -2,6 +2,7 @@
 NEWTOPIC 
 1. Design an class for employee which takes an id and name in  a constructor and has a salaray property 
 2. Design a class for managers which has the empolyee class in it and also a department property
+3. Design the above two classes using the javascript prototypes
 */
 
 //CASE 1
@@ -48,3 +49,37 @@ const management = new managers(5, "john");
 console.log(management);
 management.setDepartment("development");
 console.log(management.getDapartment());
+
+//CASE 3          PROTOTYPES
+
+var employe = function (id, name) {
+  if (!id || !name) {
+    throw new Error("Both id and name are mandatory");
+  }
+  this.id = id;
+  this.name = name;
+};
+employe.prototype.setSalary = function (salary) {
+  this.salary = salary;
+};
+employe.prototype.getid = function () {
+  return this.id;
+};
+employe.prototype.getname = function () {
+  return this.name;
+};
+
+const em1 = new employe(9, "harry");
+console.log(em1);
+console.log(em1.getid());
+
+var manager = function (params) {
+  employe.apply(this, arguments);
+};
+manager.prototype = Object.create(employe.prototype); // to clone all the protoypes of employee to manager
+manager.prototype.constructor = manager; //to clone the constructor from employee to manager
+
+const m1 = new manager(3, "carry");
+console.log(m1, m1.getname());
+
+//  ANSWER  have to added

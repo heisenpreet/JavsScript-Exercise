ba6a2e5fe20d0fddf80d9b3e75db6f280b35224d
heisenpreet <99128454+heisenpreet@users.noreply.github.com>
2022-05-12 16:24:24 +0530

    added

--- 05. Currying fx/script.js
+++ 05. Currying fx/script.js
@@ -126,3 +126,62 @@ const getobj = curryfx((obj, prop) => obj.prop);
 const passingObj = getobj({ name: "harry", age: 56, place: "delhi" });
 console.log(passingObj("place"));
 //needs fixing
+
+
+
+
+const menu = Object.freeze([
+  {
+    name: "apples",
+    count: 50,
+    price: 234,
+  },
+  {
+    name: "oranges",
+    count: 32,
+    price: 212,
+  },
+  {
+    name: "burgers",
+    count: 20,
+    price: 324,
+  },
+]);
+
+const shopping = Object.freeze({
+  oranges: false,
+
+  colddrinks: true,
+
+  chocolate: true,
+
+  sugar: true,
+});
+
+const needMore = (state, limit, more) => {
+  return limit[more]
+    ? [...state, { name: more, count: 20, price: 321 }]
+    : state;
+};
+//currying
+const add =
+  (a) =>
+  (b) =>
+  (...c) => {
+    c.forEach((e) => (a = needMore(a, b, e)));
+    return a;
+  };
+
+const add2 = add(menu)(shopping);
+console.log(add2("colddrinks", "chocolate", "oranges")); //with currying
+console.log(add2("sugar"));
+
+//without currying-not ideal
+let addss = menu;
+for (let item in shopping) {
+  addss = needMore(addss, shopping, item);
+}
+
+console.log(addss);
+
+console.log(menu);
